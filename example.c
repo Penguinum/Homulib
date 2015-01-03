0c1e24e685871b23427277c603562eaa1f510676
Penguinum <penguinum@gmail.com>
2015-01-03 18:17:50 +0300

    update examples

--- example.c
+++ example.c
@@ -2,18 +2,25 @@
 #include "src/cwrapper.h"
 
 int main() {
+    // Create and init sinewave generator.
     void *gen = SinewaveGenerator_create(41000);
+    // Create and init adsr effect.
     void *adsr = ADSR_create(41000);
-
-    ADSR_start(adsr);
+    // Set parameters.
     ADSR_setAttack(adsr, 0.2);
     ADSR_setDecay(adsr, 0.3);
     ADSR_setSustain(adsr, 0.7);
     ADSR_setRelease(adsr, 0.5);
 
+    // Start producing sinewave with frequency 150 Hz.
     SinewaveGenerator_start(gen, 150);
+
+    // Start ADSR.
+    ADSR_start(adsr);
     
+    // Produce sine wave with adsr while adsr is not finished.
     while(! ADSR_finished(adsr)) {
+        // we stop sustain after 0.7 seconds of producing values
         if (ADSR_secondsPlayed(adsr) >= 0.7) {
             ADSR_stopSustain(adsr);
         }
--- example.cpp
+++ example.cpp
@@ -1,26 +1,26 @@
 #include <stdio.h>
-#include "SinewaveGenerator.h"
-#include "ADSR.h"
+#include "src/SinewaveGenerator.h"
+#include "src/ADSR.h"
 
 int main() {
     const size_t sample_rate = 41000;
-    // create and init sinewave generator 
+    // Create and init sinewave generator.
     homu::SinewaveGenerator g(sample_rate);
-    // create and init adsr effect
+    // Create and init adsr effect.
     homu::ADSR adsr(sample_rate);
-    // set parameters
+    // Set parameters.
     adsr.setAttack(0.2);
     adsr.setDecay(0.3);
     adsr.setSustain(0.7);
     adsr.setRelease(0.5);
     
-    // Start producing sinewave with frequency 150 Hz
+    // Start producing sinewave with frequency 150 Hz.
     g.start(150);
 
     // Start ADSR.
     adsr.start();
 
-    // produce sine wave with adsr while adsr is not finished
+    // Produce sine wave with adsr while adsr is not finished.
     while (! adsr.finished()) {
         // we stop sustain after 0.7 seconds of producing values
         if (adsr.secondsPlayed() >= 0.7) {
