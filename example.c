7321042217b012e69553f907eb6a475a349280ea
Penguinum <penguinum@gmail.com>
2015-01-06 22:29:37 +0300

    Renames in C wrapper.

--- example.c
+++ example.c
@@ -3,31 +3,31 @@
 
 int main() {
     // Create and init sinewave generator.
-    void *gen = SinewaveGenerator_create(41000);
+    void *gen = SinewaveGenerator_Create(41000);
     // Create and init adsr effect.
-    void *adsr = ADSR_create(41000);
+    void *adsr = ADSR_Create(41000);
     // Set parameters.
-    ADSR_setAttack(adsr, 0.2);
-    ADSR_setDecay(adsr, 0.3);
-    ADSR_setSustain(adsr, 0.7);
-    ADSR_setRelease(adsr, 0.5);
+    ADSR_SetAttack(adsr, 0.2);
+    ADSR_SetDecay(adsr, 0.3);
+    ADSR_SetSustain(adsr, 0.7);
+    ADSR_SetRelease(adsr, 0.5);
 
     // Start producing sinewave with frequency 150 Hz.
-    SinewaveGenerator_start(gen, 150);
+    SinewaveGenerator_Start(gen, 150);
 
     // Start ADSR.
-    ADSR_start(adsr);
+    ADSR_Start(adsr);
     
     // Produce sine wave with adsr while adsr is not finished.
-    while(! ADSR_finished(adsr)) {
+    while(! ADSR_Finished(adsr)) {
         // we stop sustain after 0.7 seconds of producing values
-        if (ADSR_secondsPlayed(adsr) >= 0.7) {
-            ADSR_stopSustain(adsr);
+        if (ADSR_SecondsPlayed(adsr) >= 0.7) {
+            ADSR_StopSustain(adsr);
         }
-        printf("%f\n", ADSR_nextSample(adsr) * SinewaveGenerator_nextSample(gen));
+        printf("%f\n", ADSR_NextSample(adsr) * SinewaveGenerator_NextSample(gen));
     }
 
-    SinewaveGenerator_destroy(gen);
-    ADSR_destroy(adsr);
+    SinewaveGenerator_Destroy(gen);
+    ADSR_Destroy(adsr);
     return 0;
 }
